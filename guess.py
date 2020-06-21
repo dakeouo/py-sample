c9f8ef75cb597999eba18d5ac7a48454717734c9
Person A <dake852@gmail.com>
2020-06-21 20:47:48 +0800

    UI init

---
+++ guess.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window = tk.Tk() #窗口物件
+window.title('終極密碼猜數字(000~999)') #窗口名字
+window.geometry('360x480') #窗口大小(寬X高)
+window.resizable(False, False) #禁止變更視窗大小
+
+window.mainloop()
